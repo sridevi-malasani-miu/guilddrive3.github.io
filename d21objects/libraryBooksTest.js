4e1d859ec4535a570c193c3cac6198c14498d099
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-07 01:57:15 +0530

    Update libraryBooksTest.js

--- d21objects/libraryBooksTest.js
+++ d21objects/libraryBooksTest.js
@@ -4,7 +4,6 @@ const assert = require("assert");  //always need this with node
 const myExports = require("./app.js");  //with node need the name of your file with your functions here
 const findTitles = myExports.findTitles;  //do this for all of the functions used in the Mocha tests
 
-
 /* global assert findTitles findAuthors findIDs addBook */
 /*
 Write a JavaScript program that will accept title, author, and libraryID values from an HTML page and create new book objects for each entry.  
@@ -13,7 +12,6 @@ o	addBook, which will take title, author, and libraryID as inputs.  It will crea
 o	findTitles, which will find all the book titles in libraryBooks and return them in an alphabetically ordered array.
 o	findAuthors, which will find all the authors in libraryBooks and return them in an alphabetically ordered array.
 o	findIDs, which will find all the libraryIDs in libraryBooks and return them in an alphabetically ordered array.
-
 */
 
 describe("library", function () {
@@ -27,7 +25,8 @@ describe("library", function () {
 
     const titles = ["Mockingjay: The Final Book of The Hunger Games", "The Road Ahead", "Walter Isaacson"];
     const authors = ["Bill Gates", "Steve Jobs", "Suzanne Collins"];
-    const ids = [1254, 3245, 4264];
+   // const ids = [1254, 3245, 4264];
+   const ids = [1254,4264, 3245];
 
 
     it("find titles", function () {
@@ -45,7 +44,8 @@ describe("library", function () {
     it("add book", function () {
         const newBook = {title: "My New Book", author: "Me Too", libraryID: 1144};
         ids.push(1144);
-        assert.deepEqual(addBook(newBook), newBook);
+       // assert.deepEqual(addBook(newBook), newBook);
+       assert.deepEqual(addBook( "My New Book","Me Too",1144), newBook);
         assert.deepEqual(findIDs(), ids);
     });
 
