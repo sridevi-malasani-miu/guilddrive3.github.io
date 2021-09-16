42301e8a853ec9991f7ddee625d158c9d6f1088e
sridevi-malasani-miu <89537694+sridevi-malasani-miu@users.noreply.github.com>
2021-09-16 08:20:15 +0530

    Update spreadTests.js

--- d32recursiveStructures/spreadTests.js
+++ d32recursiveStructures/spreadTests.js
@@ -28,7 +28,7 @@ describe("spread operator tests", function () {
         const oldArr = [1, 2, 3];
         const newArr = copyArray(oldArr);
         assert.deepEqual(newArr, oldArr);
-        oldArr.push[4];
+        newArr.push(4);
         assert.deepEqual(oldArr, [1, 2, 3]);
     });
 
